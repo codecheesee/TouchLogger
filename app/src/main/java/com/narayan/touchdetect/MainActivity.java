05e70a88c45795362b8498914847e47f264f10b1
codecheesee <n.gorai098@gmail.com>
2021-07-01 14:27:54 +0530

    2nd Time Commit

--- app/src/main/java/com/narayan/touchdetect/MainActivity.java
+++ app/src/main/java/com/narayan/touchdetect/MainActivity.java
@@ -1,8 +1,12 @@
 package com.narayan.touchdetect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConstraintLayout myLayout = (ConstraintLayout) findViewById(R.id.main_layout);
+        myLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("TouchEvents","Touch is detected");
+                return true;
+            }
+        });
     }
 }
